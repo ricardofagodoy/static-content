40cb2e3b2e89fa54654ee19afe0b27050e477c7b
ricardofagodoy <ricardofagodoy@gmail.com>
2019-06-24 22:42:37 -0300

    fono

---
+++ build/precache-manifest.d896510da4ff8030ddd5491b487d5f59.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86cff9ca5da4a40ef188600052b54bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e098b5bfb6c8a2e363c",
+    "url": "/static/css/main.74dba5bf.chunk.css"
+  },
+  {
+    "revision": "61dcccced0cae90ead40",
+    "url": "/static/js/2.0d823483.chunk.js"
+  },
+  {
+    "revision": "8e098b5bfb6c8a2e363c",
+    "url": "/static/js/main.f5154731.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f16616a95e83968bfcf71780c40c395f",
+    "url": "/static/media/Sacramento-Regular.f16616a9.ttf"
+  },
+  {
+    "revision": "722609b4ed80a3ebaed437766e36911c",
+    "url": "/static/media/fino.722609b4.jpg"
+  },
+  {
+    "revision": "76e4ec60907e1e71c0ec0af00cb42dfc",
+    "url": "/static/media/fino.76e4ec60.mp3"
+  },
+  {
+    "revision": "e373e6ddbaa6ca793623f4cb47910884",
+    "url": "/static/media/fundo.e373e6dd.png"
+  },
+  {
+    "revision": "96d8ff87234810fc8ef79797317233a9",
+    "url": "/static/media/grosso.96d8ff87.jpg"
+  },
+  {
+    "revision": "f36476faf23477209bd1ead99471c172",
+    "url": "/static/media/grosso.f36476fa.mp3"
+  },
+  {
+    "revision": "01f3b874ced9538172a8dcda78ac8d3a",
+    "url": "/static/media/repetir.01f3b874.jpg"
+  },
+  {
+    "revision": "7a58e02a958acfe1805d2486bf4db51d",
+    "url": "/static/media/voltar.7a58e02a.jpg"
+  }
+]);
